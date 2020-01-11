8493a58a8885b8a08d8dd23407ee51227517bfbe
Anies Rayyes <anies.rayyes@gmail.com>
2020-01-12 00:58:26 +0200

    move main table down and remove

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import MainPage from './components/MainPage/index';
+import './style.css';
 
 function App() {
   return (
-    <MainPage />
+    <div className="MainApp">
+      <MainPage />
+    </div>
   );
 }
 
